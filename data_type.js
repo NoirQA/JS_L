4c39f3e23e7d6b87d950c879787c71e51f239076
NoirQA <94997768+NoirQA@users.noreply.github.com>
2021-12-21 21:11:09 +0300

    Выгрузка проекта из консоли webstorm

---
+++ data_type.js
@@ -0,0 +1,45 @@
+// может содержать строку и чиcло
+
+let massage = 'hello'
+massage = 123456
+
+// целочисленные и с плавающе точкой
+
+let n = 123
+n = 12.345
+
+// Infinity
+
+console.log (1 / 0)
+
+// NaN
+
+console.log( 'one' / 2)
+console.log( 'one' / 2 + 5 )
+
+// BigInt
+
+const bigINt = 1234567890123456789012345678901234567890n
+
+// String
+
+let name = "Alex"
+let userTown = 'Krasnodar'
+
+// boolean
+
+let earthRout = true
+let sunBlack = false
+
+let test = 4 > 3
+console.log(test)
+
+// null
+
+let age = null
+
+// undefined
+
+let year
+
+
