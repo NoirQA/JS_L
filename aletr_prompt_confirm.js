ee3a28b295f0231832c235be795f3f280b5ad954
NoirQA <94997768+NoirQA@users.noreply.github.com>
2021-12-21 22:10:49 +0300

    закончил главу + задание

---
+++ aletr_prompt_confirm.js
@@ -0,0 +1,8 @@
+alert('Привет')
+const name = prompt('как тебя зовут?')
+alert(name)
+let age = prompt('Сколько лет', 100)
+alert(`тебе ${age} лет!`)
+let isTrue = confirm('а ты не врешь ?')
+console.log(isTrue)
+
